b074ff593b1961ac5f474135087e59881c1a464c
Hack Reactor Students <github@hackreactor.com>
2013-08-07 16:43:10 -0700

    completed queue functional shared

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,3 +1,40 @@
 var makeQueue = function(){
-	// Hey! Copy your code from src/functional/queue.js and paste it here
+  var newQueue = {};
+
+  newQueue.storage = {};
+
+  newQueue.sizeOf = 0;
+
+  newQueue.position = 0;
+
+  _.extend(newQueue, makeQueue.queueMethods);
+
+  return newQueue;
+};
+
+makeQueue.queueMethods = {};
+
+makeQueue.queueMethods.enqueue = function(value){
+  this.sizeOf++;
+  this.storage[this.position++] = value;
+};
+
+makeQueue.queueMethods.dequeue = function(){
+  this.sizeOf && this.sizeOf--;
+  var minKey/* = Number.POSITIVE_INFINITY*/;
+  for (var key in this.storage) {
+    if (typeof minKey === 'undefined') {
+      minKey = key;
+    }
+    else if (key < minKey){
+      minKey = key;
+    }
+  }
+  var result = this.storage[minKey];
+  delete this.storage[minKey];
+  return result;
+};
+
+makeQueue.queueMethods.size = function(){
+  return this.sizeOf;
 };
