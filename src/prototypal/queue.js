2b91feecff9efd1b465925d5c8d42e4960352e22
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:12:37 -0700

    prototypal works with queue

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,3 +1,38 @@
-var Queue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var Queue = function(){
+  var newQueue = Object.create(Queue.queueMethods);
+
+  newQueue.storage = {};
+
+  newQueue.sizeOf = 0;
+
+  newQueue.position = 0;
+
+  return newQueue;
+};
+
+Queue.queueMethods = {};
+
+Queue.queueMethods.enqueue = function(value){
+  this.sizeOf++;
+  this.storage[this.position++] = value;
+};
+
+Queue.queueMethods.dequeue = function(){
+  this.sizeOf && this.sizeOf--;
+  var minKey/* = Number.POSITIVE_INFINITY*/;
+  for (var key in this.storage) {
+    if (typeof minKey === 'undefined') {
+      minKey = key;
+    }
+    else if (key < minKey){
+      minKey = key;
+    }
+  }
+  var result = this.storage[minKey];
+  delete this.storage[minKey];
+  return result;
+};
+
+Queue.queueMethods.size = function(){
+  return this.sizeOf;
 };
