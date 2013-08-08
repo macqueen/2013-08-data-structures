58a057cd77f0afde6906092d44ea418058d90044
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:26:19 -0700

    pseudoclassical for queue

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,3 +1,34 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+
+  this.storage = {};
+
+  this.sizeOf = 0;
+
+  this.position = 0;
+
+};
+
+Queue.prototype.enqueue = function(value){
+  this.sizeOf++;
+  this.storage[this.position++] = value;
+};
+
+Queue.prototype.dequeue = function(){
+  this.sizeOf && this.sizeOf--;
+  var minKey/* = Number.POSITIVE_INFINITY*/;
+  for (var key in this.storage) {
+    if (typeof minKey === 'undefined') {
+      minKey = key;
+    }
+    else if (key < minKey){
+      minKey = key;
+    }
+  }
+  var result = this.storage[minKey];
+  delete this.storage[minKey];
+  return result;
+};
+
+Queue.prototype.size = function(){
+  return this.sizeOf;
 };
