75b2aca2026fb0b23012c02eba4db23c1cf03f1a
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:03:40 -0700

    stack.js now demonstrates prototypal inheritence.

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,30 @@
-var Stack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var Stack = function(){
+
+  var newStack = Object.create(Stack.stackMethods);
+
+  newStack.storage = {};
+
+  newStack.sizeOf = 0;
+
+  newStack.stack = {};
+
+  return newStack;
+};
+
+Stack.stackMethods = {};
+
+// define makeStack methods
+Stack.stackMethods.push = function(value){
+  this.storage[this.sizeOf++] = value;
+};
+
+Stack.stackMethods.pop = function(){
+  this.sizeOf && this.sizeOf--;
+  var candidate = this.storage[this.sizeOf];
+  delete this.storage[this.sizeOf];
+  return candidate;
+};
+
+Stack.stackMethods.size = function(){
+  return this.sizeOf;
 };
